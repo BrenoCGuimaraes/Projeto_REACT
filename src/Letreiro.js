2f84626a50a269ddda31a6147c374f4fd89bafd7
Breno Guimarães <161730901+BrenoCGuimaraes@users.noreply.github.com>
2024-05-01 19:43:45 -0300

    Update Letreiro.js

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,11 +1,22 @@
 
 function Letreiro() {
+    const [text, setText] = useState("Meu Letreiro");
+    
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            setText((textAnterior) =>{
+                if(textAnterior == "Meu Letreiro") return "Primeira mensagem";
+                else if (textAnterior === "Primeira mensagem") return "Segunda mensagem";
+                else return "Meu Letreiro";
+            });
+        }, 2000);
+        return () => clearInterval (interval);
+    }, []);
     return (
-      <>
-        <h1>Meu Letreiro</h1>
-      </>
+        <>
+            <h1>{text}</h1>
+        </>
     );
   }
-  
   export default Letreiro;
   
