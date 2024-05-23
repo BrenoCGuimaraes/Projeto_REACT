4de924892a79ac3882cd0fbd77fdc5033d89ef9f
Breno Guimarães <161730901+BrenoCGuimaraes@users.noreply.github.com>
2024-05-23 10:15:51 -0300

    correção das atividades

---
+++ atividade03/atividade3.js
@@ -0,0 +1,36 @@
+import Gallery from "./Gallery";
+import TodoList from "./TodoList";
+import Avatar from './Avatar';
+import Scientist from './Scientist';
+import Avatar1 from './Avatar1';
+import Avatar2 from './Avatar2';
+import People from './People';
+import Persons from './Persons';
+import Recipe from './Recipe';
+import TeaSet from "./TeaSet";
+import Item from "./Item";
+import { Link } from "react-router-dom";
+import App from "./App";
+
+export default function Atividade3() {
+    return (
+        <>
+            <h1>Atividade 03</h1>
+            {/* <Profile /> */}
+            <Gallery/>
+            <Avatar/>
+            <Avatar1/>
+            <Avatar2/>
+            <People />
+            <Persons/>
+            <Recipe/>
+            <Scientist/>
+            <TodoList />
+            <Item />
+            <App />
+            <TeaSet/>
+            <br />
+            <Link to="/">retornar a página inicial</Link>
+        </>
+    )
+}
